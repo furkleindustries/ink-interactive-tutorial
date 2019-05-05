d59f17df1eff48152b3c20a80aee7d284083950b
furkle <furkle@furkleindustries.com>
2019-05-04 23:18:10 -0400

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  default as React,
+  lazy,
+  Suspense,
+} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Content = lazy(() => import('./Content.mdx'));
 
-export default App;
+export const App =  () => (
+  <div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Content />
+    </Suspense>
+  </div>
+);
